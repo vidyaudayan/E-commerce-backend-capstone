2d8d04cb564b64e7a4ef1c4902d4c4d727e4be1c
vidyaudayan <vidyaudayan89@gmail.com>
2024-06-19 16:11:06 +0530

    modified route

--- routes/paymentRoutes.js
+++ routes/paymentRoutes.js
@@ -2,12 +2,14 @@ import express from "express";
 import  {createPayment, getPaymentById} from '../controllers/paymentController.js';
 import authenticateUser from "../middlewares/user-middleware.js";
 import cors from 'cors'
+
+const Router = express.Router();
+const paymentRouter = express.Router();
+
 paymentRouter.use(cors({
     origin: 'https://imaginative-genie-54ec39.netlify.app' ,
     credentials: true,    
 }))
-const Router = express.Router();
-const paymentRouter = express.Router();
 
 paymentRouter.use("/payment",paymentRouter)
 paymentRouter.post("/",authenticateUser, createPayment);
